62eda41f6d7b107e10211238a28334224b5a4027
baswarajmamidgi <baswarajmamidgi@gmail.com>
2017-09-21 08:56:02 +0530

    first commit

---
+++ Eyedetection.py
@@ -0,0 +1,31 @@
+import cv2
+
+"""
+Baswaraj mamidgi
+21-9-2017
+"""
+
+
+
+eye_cascade = cv2.CascadeClassifier('data/cascade.xml')
+
+cap = cv2.VideoCapture(0)
+
+while 1:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    eyes = eye_cascade.detectMultiScale(gray,50,50)
+
+    for (x, y, w, h) in eyes:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(img, 'eye', (x, y), font, 0.5,(0,255,255),2,cv2.LINE_AA)
+
+
+    cv2.imshow('img', img)
+    k = cv2.waitKey(1) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
